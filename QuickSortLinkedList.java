0bdf0c8584aa5839ccff675fe0055036e267b718
daniel5115 <daniel811815@gmail.com>
2021-05-29 11:21:04 -0500

    update master

---
+++ QuickSortLinkedList.java
@@ -0,0 +1,93 @@
+// Java program for Quick Sort on Singly Linled List
+
+/*sort a linked list using quick sort*/
+public
+class QuickSortLinkedList {
+    // takes first and last node,
+    // but do not break any links in
+    // the whole linked list
+    node paritionLast(node start, node end)
+    {
+        if (start == end || start == null || end == null)
+            return start;
+
+        Node pivot_prev = start;
+        Node curr = start;
+        int pivot = end.data;
+
+        // iterate till one before the end,
+        // no need to iterate till the end
+        // because end is pivot
+        while (start != end) {
+            if (start.data < pivot) {
+                // keep tracks of last modified item
+                pivot_prev = curr;
+                int temp = curr.data;
+                curr.data = start.data;
+                start.data = temp;
+                curr = curr.next;
+            }
+            start = start.next;
+        }
+
+        // swap the position of curr i.e.
+        // next suitable index and pivot
+        int temp = curr.data;
+        curr.data = pivot;
+        end.data = temp;
+
+        // return one previous to current
+        // because current is now pointing to pivot
+        return pivot_prev;
+    }
+
+    void sort(node start, node end)
+    {
+        if(start == null || start == end|| start == end.next )
+            return;
+
+        // split list and partion recurse
+        Node pivot_prev = paritionLast(start, end);
+        sort(start, pivot_prev);
+
+        // if pivot is picked and moved to the start,
+        // that means start and pivot is same
+        // so pick from next of pivot
+        if (pivot_prev != null && pivot_prev == start)
+            sort(pivot_prev.next, end);
+
+        // if pivot is in between of the list,
+        // start from next of pivot,
+        // since we have pivot_prev, so we move two nodes
+        else if (pivot_prev != null
+                 && pivot_prev.next != null)
+            sort(pivot_prev.next.next, end);
+    }
+
+    // Driver Code
+public
+    static void main(String[] args)
+    {
+        QuickSortLinkedList list
+            = new QuickSortLinkedList();
+        list.addNode(30);
+        list.addNode(3);
+        list.addNode(4);
+        list.addNode(20);
+        list.addNode(5);
+
+        Node n = list.head;
+        while (n.next != null)
+            n = n.next;
+
+        System.out.println("Linked List before sorting");
+        list.printList(list.head);
+
+        list.sort(list.head, n);
+
+        System.out.println("\nLinked List after sorting");
+        list.printList(list.head);
+    }
+}
+
+// This code is contributed by trinadumca
